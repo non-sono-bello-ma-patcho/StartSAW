e3ca3669a2fdfbaa17b2a1db6d46b01aee3379df
rollingflamingo <andrea.storace.uni@gmail.com>
2018-12-21 13:32:18 +0100

    essential elements

--- index.php
+++ index.php
@@ -3,5 +3,131 @@
  * Created by PhpStorm.
  * User: phibonachos
  * Date: 17/12/18
- * Time: 11.50
+ * Time: 14.11
  */
+
+?>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Phibonachos</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="apple-touch-icon" href="favicon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/main.css">
+        <link href="https://fonts.googleapis.com/css?family=Catamaran:300|Concert+One" rel="stylesheet">
+    </head>
+
+<body id>
+    <div class="global-header">
+        <div class="banner">
+            <div class="titlediv main">
+                <h1>Herschel</h1>
+            </div>
+        </div>
+        <div class="titlediv sub">
+            <blockquote>
+                That’s the advantage of space.  It’s big enough to hold practically anything, and so, eventually, it does.
+                <figcaption>-T. Pratchett</figcaption>
+            </blockquote>
+        </div>
+    </div>
+    <div id="planettest">
+        <svg  viewBox="0 0 1802 1533">
+            <!-- Deepest comet level -->
+            <g transform="translate(200, 200), rotate(45)">
+              <path d="
+                        M0,20
+                        L0,100
+                        C0,103.7 5,103.7 5,100
+                        L5,180
+                        C5,183.7 10,183.7 10,180
+                        L10,120
+                        C10,123.7 15,123.7 15,120
+                        L15,110
+                        C15,113.7 20,113.7 20,110
+                        L20,20
+                        Z
+                      "
+                    fill="#172535"
+              ></path>
+            </g>
+            <!-- Medium comet layer -->
+            <g transform="translate(200, 200), rotate(45)">
+                <path d="
+                        M0,20
+                        L0,80
+                        C0,83.7 5,83.7 5,80
+                        L5,90
+                        C5,93.7 10,93.7 10,90
+                        L10,110
+                        C10,113.7 15,113.7 15,110
+                        L15,95
+                        C15,98.7 20,98.7 20,95
+                        L20,20
+                        Z
+                        "
+                      fill="#2e3b49"
+                ></path>
+            </g>
+            <!-- Top comet layer -->
+            <g transform="translate(200, 200), rotate(45)">
+                <path d="
+                        M0,20
+                        L0,60
+                        C0,63.7 5,63.7 5,60
+                        L5,80
+                        C5,83.7 10,83.7 10,80
+                        L10,70
+                        C10,73.7 15,73.7 15,70
+                        L15,60
+                        C15,63.7 20,63.7 20,60
+                        L20,20
+                        C20,5 0,5 0,20
+                        Z
+                        "
+                      fill="#737c85"
+                ></path>
+            </g>
+        </svg>
+    </div>
+
+    <div class="descriptiondiv">
+        <h1>Chi siamo</h1>
+        <p>Herschel è la prima piattaforma che ti permette di prenotare le tue vacanze extra terrestri!</p>
+        <pre>
+            La memoria è venuta meno
+            per fare spazio a poche parole chiave,
+            ave avi,
+            abbiamo dimenticato i vostri racconti
+            Esterno il ricordo
+            comune il sapere certe le fonti
+            comodo, domotico, modifico
+            la casa stando fermo
+            Condizionare l'aria in terza persona
+            non sento più freddo non provo dolore
+            sono e non sono un giovane
+            illuminato
+            a risparmio energetico
+            io sono un giovane illuminato
+            da una realtà che non conosco più
+        </pre>
+    </div>
+    <div class="productpreview">
+    </div>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
+M587.352079,83.7275077
+C586.630896,82.5219101 585.032928,82.1091595 583.785404,82.8066558
+L576.702117,86.7630533
+C575.45324,87.4592435 575.025672,89.0018336 575.748207,90.2061249
+C576.46939,91.4117225 578.067358,91.8244731 579.316236,91.1269768
+L586.399522,87.1705793
+C587.645693,86.4743891 588.073261,84.9331052 587.352079,83.7275077
