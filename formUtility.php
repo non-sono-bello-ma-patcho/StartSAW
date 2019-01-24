b1845d239f808d0b4fd7fcc1c91129211de47b71
rollingflamingo <andrea.storace.uni@gmail.com>
2019-01-24 20:58:19 +0100

    Added ajax form async username check

---
+++ formUtility.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: phibonachos
+ * Date: 24/01/19
+ * Time: 20.23
+ */
+
+require 'databaseUtility.php';
+
+$username = trim($_REQUEST["username"]);
+
+echo get_information("users", "username", "username", $username) != "";
+
+// obtain data from request
