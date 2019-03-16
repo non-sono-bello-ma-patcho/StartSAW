64d7f8b7a8e826c1c9f2fb5281945ef4f9006526
Alessandro <alex.savona97@gmail.com>
2019-03-16 16:51:23 +0100

    added wishlistUtility.php

---
+++ wishlistUtility.php
@@ -0,0 +1,16 @@
+<?php
+require "databaseUtility.php";
+
+function getUserWishList($username){
+    return get_information_listed("wishlist","item","username",$username);
+}
+
+function removeFromWishList($username,$item){
+    row_deletion("wishlist",array("username","item"),array($username,$item));
+}
+
+function insertUserWishList($username,$item){
+    if(in_array($item,getUserWishList($username)))
+        return;
+    else row_insertion("wishlist",array($username,$item));
+}
