801221434316e6ea66d8c3f6599995d5931a19f0
Andrea Storace <a.storace@wikicasa.it>
2019-06-17 23:37:02 +0200

    implementing db connection with PDO

--- PDO/Products.php
+++ PDO/Products.php
@@ -4,7 +4,59 @@
 namespace PDO;
 
 
-class Products
-{
+class Products{
+    private $conn;
 
+
+    private $code;
+    private $name;
+    private $description;
+    private $price;
+    private $img;
+    private $relevance;
+    private $level;
+    private $location;
+    private $admin;
+
+    public function __construct(Database $db){
+        $this->conn = $db->getConnection();
+    }
+
+    function create(){
+        // insert query
+        $query = "INSERT INTO users values (:name, :price, :code, :relevance, :pswd, :img, :description, :location, :admin)";
+
+        // prepare the query
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $this->code=htmlspecialchars(strip_tags($this->code));
+        $this->name=htmlspecialchars(strip_tags($this->name));
+        $this->description=htmlspecialchars(strip_tags($this->description));
+        $this->price=htmlspecialchars(strip_tags($this->price));
+        $this->img=htmlspecialchars(strip_tags($this->img));
+        $this->relevance=htmlspecialchars(strip_tags($this->relevance));
+        $this->level=htmlspecialchars(strip_tags($this->level));
+        $this->location=htmlspecialchars(strip_tags($this->location));
+        $this->admin=htmlspecialchars(strip_tags($this->admin));
+
+        // bind the values
+        $stmt->bindParam(':code', $this->code);
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':description', $this->description);
+        $stmt->bindParam(':price', $this->price);
+        $stmt->bindParam(':img', $this->img);
+        $stmt->bindParam(':relevance', $this->relevance);
+        $stmt->bindParam(':level', $this->level);
+        $stmt->bindParam(':location', $this->location);
+        $stmt->bindParam(':admin', $this->admin);
+
+
+        // execute the query, also check if query was successful
+        if($stmt->execute()){
+            return true;
+        }
+
+        return false;
+    }
 }
--- PDO/Users.php
+++ PDO/Users.php
@@ -4,7 +4,62 @@
 namespace PDO;
 
 
-class Users
-{
+class Users{
+    private $conn;
+    private $tableName = "users";
 
+    private $username;
+    private $name;
+    private $surname;
+    private $email;
+    private $password;
+    private $img;
+    private $description;
+    private $location;
+    private $admin;
+
+    public function __construct(Database $db){
+        $this->conn = $db->getConnection();
+    }
+
+    function create(){
+        // insert query
+        $query = "INSERT INTO users values (:name, :surname, :username, :email, :pswd, :img, :description, :location, :admin)";
+
+        // prepare the query
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $this->name=htmlspecialchars(strip_tags($this->name));
+        $this->surname=htmlspecialchars(strip_tags($this->surname));
+        $this->username=htmlspecialchars(strip_tags($this->username));
+        $this->email=htmlspecialchars(strip_tags($this->email));
+        $this->password=htmlspecialchars(strip_tags($this->password));
+        $this->img=htmlspecialchars(strip_tags($this->img));
+        $this->description=htmlspecialchars(strip_tags($this->description));
+        $this->location=htmlspecialchars(strip_tags($this->location));
+        $this->admin=htmlspecialchars(strip_tags($this->admin));
+
+        // bind the values
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':surname', $this->surname);
+        $stmt->bindParam(':username', $this->username);
+        $stmt->bindParam(':email', $this->email);
+        $stmt->bindParam(':pswd', $this->password);
+        $stmt->bindParam(':img', $this->img);
+        $stmt->bindParam(':description', $this->description);
+        $stmt->bindParam(':location', $this->location);
+        $stmt->bindParam(':admin', $this->admin);
+
+        // hash the password before saving to database
+        $password_hash = password_hash($this->password, PASSWORD_BCRYPT);
+        $stmt->bindParam(':password', $password_hash);
+
+        // execute the query, also check if query was successful
+        if($stmt->execute()){
+            return true;
+        }
+
+        return false;
+    }
 }
